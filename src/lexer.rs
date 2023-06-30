3f041e204b6865d9998484692335b72d4c0b72c3
Rigo <rigothedev@gmail.com>
2023-06-29 19:42:08 -0700

    Start creating the lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -29,3 +29,35 @@ enum Token {
     // Misc
     Identifier(String),
 }
+
+struct Lexer {
+    input: Vec<u8>,
+    character: u8,
+    current_position: usize,
+    next_position: usize,
+}
+
+impl Lexer {
+    fn new(input: String) -> Lexer {
+        let mut lexer = Self {
+            input: input.into_bytes(),
+            character: 0,
+            current_position: 0,
+            next_position: 0,
+        };
+
+        lexer.read_character();
+        lexer
+    }
+
+    fn read_character(&mut self) {
+        if self.next_position >= self.input.len() {
+            self.character = 0;
+        } else {
+            self.character = self.input[self.next_position];
+        }
+
+        self.current_position = self.next_position;
+        self.next_position += 1;
+    }
+}
